7139a5d7fe2d8dac6db21022d1537b64dc63d55b
Taylor Isbell <taylor.isbell@databricks.com>
2024-02-29 11:29:56 -0500

    moved gpu nodepool provisioning

--- provision-aks.sh
+++ provision-aks.sh
@@ -10,16 +10,4 @@ az aks create \
   --min-count 1 \
   --max-count 20 \
   --generate-ssh-keys
-az aks nodepool add \
-  --resource-group ${RESOURCE_GROUP} \
-  --cluster-name ${CLUSTER_NAME} \
-  --name gpunp \
-  --node-count 1 \
-  --node-vm-size Standard_NC6s_v3 \
-  --node-taints sku=gpu:NoSchedule \
-  --labels agentpool=gpunp \
-  --aks-custom-headers UseGPUDedicatedVHD=true \
-  --enable-cluster-autoscaler \
-  --min-count 1 \
-  --max-count 3
 az aks get-credentials --resource-group ${RESOURCE_GROUP} --name ${CLUSTER_NAME}
