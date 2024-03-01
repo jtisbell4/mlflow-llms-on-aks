9bb3b7298604442af0e7bc8ce07cfc2b5122cd12
Taylor Isbell <taylor.isbell@databricks.com>
2024-03-01 17:11:55 -0500

    stupid token

--- download_mlflow_artifacts.py
+++ download_mlflow_artifacts.py
@@ -1,10 +1,9 @@
 import argparse
-import os
 
 import mlflow
 
 
-def main(run_id: str, dest_path: str):
+def main(run_id: str, dest_path: str = None):
     mlflow.artifacts.download_artifacts(run_id=run_id, dst_path=dest_path)
 
 
@@ -17,10 +16,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dest-path",
         type=str,
+        default=None
     )
     args = parser.parse_args()
-
-    print(os.environ["DATABRICKS_TOKEN"])
-    print(os.environ["DATABRICKS_HOST"])
 
     main(run_id=args.run_id, dest_path=args.dest_path)
