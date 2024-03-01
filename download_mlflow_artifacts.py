b558d4c192bb11e390ac0e94123c0f61df54ff1b
Taylor Isbell <taylor.isbell@databricks.com>
2024-03-01 14:54:41 -0500

    download_mlflow_artfacts.py

---
+++ download_mlflow_artifacts.py
@@ -0,0 +1,22 @@
+import argparse
+
+import mlflow
+
+
+def main(run_id: str, dest_path: str):
+    mlflow.artifacts.download_artifacts(run_id=run_id, dst_path=dest_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--run-id",
+        type=str,
+    )
+    parser.add_argument(
+        "--dest-path",
+        type=str,
+    )
+    args = parser.parse_args()
+
+    main(run_id=args.run_id, dest_path=args.dest_path)
