d0378a3337b7b0b0bfd0c0a77f2eec42f73c1dab
Taylor Isbell <taylor.isbell@databricks.com>
2024-03-08 07:42:27 -0600

    can't get cuda-toolkit installed correctly but everything else is working

--- deploy-model.sh
+++ deploy-model.sh
@@ -1,3 +1,4 @@
 kubectl get namespace seldon || kubectl create namespace seldon
 kubectl apply -f manifests/secret.yaml
 kubectl apply -f manifests/seldon-llama.yaml
+kubectl apply -f manifests/seldon-test.yaml
